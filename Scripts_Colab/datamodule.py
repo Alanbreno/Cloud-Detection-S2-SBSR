55b9b984dc8bc6d3f2140273b50820d5057dc9cf
Alan Breno Correa <alanbrenocorrea@gmail.com>
2024-11-20 22:41:21 -0300

    Modificações

--- Scripts_Colab/datamodule.py
+++ Scripts_Colab/datamodule.py
@@ -4,6 +4,7 @@ import pytorch_lightning as pl
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 from dataset import CoreDataset
+import mlstac
 
 # Pipeline de augmentations com Shift e Rotação
 augmentation_pipeline = A.Compose(
@@ -19,18 +20,18 @@ augmentation_pipeline = A.Compose(
 class CoreDataModule(pl.LightningDataModule):
     def __init__(
         self,
-        dataframe: pd.DataFrame,
         batch_size: int = 4,
         train_index_mask: int = 13,
         val_index_mask: int = 13,
         test_index_mask: int = 13,
     ):
         super().__init__()
-
+        data = mlstac.load(snippet="./drive/MyDrive/CloudSen12+/main.json").metadata
+        
         # Separar o DataFrame em datasets de treino, validação e teste
-        self.train_dataset = dataframe[dataframe["set_type"] == "train"]
-        self.validation_dataset = dataframe[dataframe["set_type"] == "val"]
-        self.test_dataset = dataframe[dataframe["set_type"] == "test"]
+        self.train_dataset = data[(data['proj_shape'] == 509) & (data['label_type'] == 'high')& (data['split'] == 'train')]
+        self.validation_dataset = data[(data['proj_shape'] == 509) & (data['label_type'] == 'high')& (data['split'] == 'validation')]
+        self.test_dataset = data[(data['proj_shape'] == 509) & (data['label_type'] == 'high')& (data['split'] == 'test')]
 
         # Definir o batch_size
         self.batch_size = batch_size
--- Scripts_Colab/dataset.py
+++ Scripts_Colab/dataset.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
 from torch.utils.data import Dataset
-import rasterio
+import config
+import mlstac
 
 
 class CoreDataset(Dataset):
@@ -10,22 +11,24 @@ class CoreDataset(Dataset):
         self.subset = subset
         self.index_mask = index_mask
         self.augmentations = augmentations
-
+        estatisticas = np.load(config.DIR_BASE + "CloudSen12+/estatisticas_dataset_512_high_train.npz")
+        self.medias = estatisticas["medias"]
+        self.desvios_padroes = estatisticas["desvios_padroes"]
+        
     def __len__(self):
         return len(self.subset)
 
     def __getitem__(self, index: int):
         # Obter o caminho do arquivo a partir do DataFrame
-        img_path = self.subset.iloc[index]["file_path"]
-
-        # Lê todas as bandas da imagem
-        bandas = rasterio.open(img_path).read()
-
-        # Transforma em array numpy
-        bandas = np.array(bandas)
-
+        bandas = mlstac.get_data(dataset=self.subset.iloc[index], quiet=False).squeeze()
+        
         # Assumindo que as bandas estão nos primeiros canais
-        X = bandas[0:13, :, :].astype(np.float32) / 10000
+        X = bandas[0:13, :, :].astype(np.float32)
+        imagem_normalizada = np.zeros_like(X)
+        for banda in range(13):
+            imagem_normalizada[banda, :, :] = (X[banda, :, :] - self.medias[banda]) / self.desvios_padroes[banda]
+            
+        X = imagem_normalizada
 
         # Assumindo que o alvo está no canal 14 (index 13)
         y = bandas[self.index_mask, :, :].astype(np.int64)
